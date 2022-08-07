33c29c664563a4b52972612678ca651ded062cae
hezirel <80560775+hezirel@users.noreply.github.com>
2022-08-07 14:25:47 +0200

    FIX

--- src/components/app/clock/Clock.jsx
+++ src/components/app/clock/Clock.jsx
@@ -1,11 +1,15 @@
-import { $CombinedState } from "@reduxjs/toolkit";
 import {
 	React,
 } from "react";
 
 import {
+	useDispatch,
+	useSelector
+} from "react-redux";
+
+import {
 	setBreak,
-} from "../../redux/features/clockSlice";
+} from "../../../redux/features/clockSlice";
 
 import "./Clock.css";
 
@@ -16,33 +20,33 @@ function Clock() {
 	const breakLength = useSelector(state => state.clock.break);
 	const sessionLength = useSelector(state => state.clock.session);
 	const handleBreak = (e) => {
-		dispatch(setBreak(e.target.value === '-' ? breakLength - 1 : breakLength + 1));
+		dispatch(setBreak(e.target.value === "-" ? breakLength - 1 : breakLength + 1));
 	};
 
 	return (
-	<div id="app">
-		<div id="break">
-			<div id="break-length">{breakLength}</div>
-			<div id="break-label">Break Length</div>
-			<button id="break-decrement" onClick={handleBreak}>-</button>
-			<button id="break-increment" onClick={handleBreak}>+</button>
+		<div id="app">
+			<div id="break">
+				<div id="break-length">{breakLength}</div>
+				<div id="break-label">Break Length</div>
+				<button id="break-decrement" onClick={handleBreak}>-</button>
+				<button id="break-increment" onClick={handleBreak}>+</button>
+			</div>
+			<div id="session">
+				<div id="session-length">{sessionLength}</div>
+				<div id="session-label">Session Length</div>
+				<button id="session-decrement">-</button>
+				<button id="session-increment">+</button>
+			</div>
+			<div id="timer">
+				<div id="timer-label">Timer Length</div>
+				<div id="timer-left">mm:ss</div>
+			</div>
+			<div id="controls">
+				<button id="start_stop">Start timer</button>
+				<button id="reset">Reset</button>
+			</div>
 		</div>
-		<div id="session">
-			<div id="session-length">{sessionLength}</div>
-			<div id="session-label">Session Length</div>
-			<button id="session-decrement">-</button>
-			<button id="session-increment">+</button>
-		</div>
-		<div id="timer">
-			<div id="timer-label">Timer Length</div>
-			<div id="timer-left">mm:ss</div>
-		</div>
-		<div id="controls">
-			<button id="start_stop">Start timer</button>
-			<button id="reset">Reset</button>
-		</div>
-	</div>
-	)
+	);
 }
 
 export default Clock;
